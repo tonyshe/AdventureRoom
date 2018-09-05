25fa02425fb4e9301a78d81618b3749fbdba7e6c
Tony She <tony.she@outlook.com>
2018-09-05 12:13:42 -0700

    Add files via upload
    cleaned up some stuff
    
    split main.js into two files: one for the universal functions, and a populate.js to furnish the room and add ad-hoc methods to certain objects.

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ function command(userCom) {
 	userCom = userCom.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,""); 	//remove punctuation
 	userCom = userCom.replace(" the ",""); 	//remove articles
 	userCom = userCom.replace(" a ",""); 	//remove articles
-    var splitCommands = splitCommandToWords(userCom); //split command to word array
+    var splitCommands = userCom.split(/\s+/); //split command to word array
     
     console.log("command: "+ splitCommands + "," + " first word: " + splitCommands[0]); //for testing purposes
     
@@ -32,9 +32,6 @@ function command(userCom) {
                     case "room":
                         newMessage(_room.describeVerbose());
                         break;
-                    case "door":
-                    	newMessage('There appears to be no door in this room. That might be a little concerning.');
-                    	break;
                     case "self":
                     case "yourself":
                     case "you":
@@ -43,7 +40,7 @@ function command(userCom) {
                         newMessage("Hey look it's you.");
                         break;
                     default:
-                    	var examineObj = findObjByName(objString)
+                    	var examineObj = findObjByNameInRoom(objString)
                         if (examineObj == 0) {
                         	newMessage("No such thing exists.");
                         }
@@ -77,7 +74,7 @@ function command(userCom) {
                         newMessage("I think you technically have that already.");
                         break;
                     default:
-                    	var takeObj = findObjByName(objString)
+                    	var takeObj = findObjByNameInRoom(objString)
                         if (takeObj == 0) {
                         	newMessage("No such thing exists.");
                         }
@@ -106,15 +103,15 @@ function command(userCom) {
             };
             break;
         case "put":
-        	var putObj = userCom.match(/put (.*) on/);
+        	var putObj = userCom.match(/put (.*) on/); //the object being moved
         	if (putObj == null) {newMessage('Please specify what to put that on.');}
         	else {
         		putObj = putObj[1]
-        		var newContainerString = userCom.match(/put (.*) on (.*)/)[2];
+        		var newContainerString = userCom.match(/put (.*) on (.*)/)[2]; //the object that will contain putObj
         		if (newContainerString == null) {newMessage('Please specify what to put that on.');}
         		else {
-        			var findObjResult = findObjByName(putObj);
-        			var findNewContainerResult = findObjByName(newContainerString);
+        			var findObjResult = findObjByNameInRoom(putObj);
+        			var findNewContainerResult = findObjByNameInRoom(newContainerString);
         			if (typeof findObjResult == 'number') {
         				if (findObjResult == 0) {
         					newMessage('No such thing exists.');
@@ -133,9 +130,12 @@ function command(userCom) {
         			}
         			else {
         				if (_user.contains.includes(findObjResult)) {
-	        				var validate = findNewContainerResult.addObject(findObjResult);
-	        				if (validate == 1) {newMessage('You put the ' + findObjResult.name[0] + ' on the ' + findNewContainerResult.name[0] + '.');}
-	        				else if (validate == 0) {newMessage("That's not possible!");};
+        					if (findNewContainerResult.isContainer) {
+		        				var validate = findNewContainerResult.addObject(findObjResult);
+		        				if (validate == 1) {newMessage('You put the ' + findObjResult.name[0] + ' on the ' + findNewContainerResult.name[0] + '.');}
+		        				else if (validate == 0) {newMessage("That's not possible!");};
+		        			}
+		        			else {newMessage("That's not possible!");};
 	        			}
 	        			else {newMessage("You don't have that!")};
         			};
@@ -147,6 +147,8 @@ function command(userCom) {
 		case "fuck":
 			newMessage("No need to be rude!");
 			break;
+		case "":
+			break;
 		default:
 			newMessage(capitalizeFirstLetter(splitCommands[0]) + " is not a command I recognize.");
 	}; //end switch
@@ -154,6 +156,7 @@ function command(userCom) {
 
 function findObjInArrayByName(name,objArray) {
 	//finds object by name in an array of objects
+	//takes name (string), and objArray (array of objects)
 	//returns [object, index in array] if only one instance is found
 	//returns 0 if no instance is found
 	//returns -1 if multiple incidences are found
@@ -161,17 +164,17 @@ function findObjInArrayByName(name,objArray) {
     for (var i = 0; i < objArray.length; i++) {
 		for (var j = 0; j < objArray[i].name.length; j++) {
 			if (objArray[i].name[j] == name) {				
-				findResult.push([objArray[i],i]);
+				findResult.push(objArray[i],i);
 				break;
 			};
 		};
 	};
 	if (findResult.length == 0) {return 0;}
-	else if (findResult.length == 1) {return findResult;}
+	else if (findResult.length == 2) {return findResult;} //equals 2 because you push 2 elements into the array per find
 	else {return -1;};
 };
 
-function findObjByName(name) {
+function findObjByNameInRoom(name) {
 	//finds object in room by iterating through every .name array in every object in room.contains and room.inanimates
 	//returns object if found, 0 if not found, and -1 if duplicates found.
 	var examineResult = [];
@@ -245,24 +248,17 @@ function capitalizeFirstLetter(string) {
 
 function addArticle(string) {
 	//adds AN to words that start with vowels, otherwise A
-	if ("aeiou".indexOf(string.charAt(0).toLowerCase()) > -1) {
-		return "an " + string;
-	}
+	if ("aeiou".indexOf(string.charAt(0).toLowerCase()) > -1) {return "an " + string;};
 	return "a " + string;
 }
 
-function splitCommandToWords(command) {
-	//splits a string with words into an array with individual words
-    return command.split(/\s+/);
-};
-
 //actions
 
-//Obj Mixins
+//Object Mixins
 let containerMix = function(obj, allowed = []) {
 	//mixin to allow objects to contain things
 	obj.contains = [];
-	obj.container = true;
+	obj.isContainer = true;
 	obj.allowed = allowed;
 
 	obj.addObject = function(newObj) {
@@ -270,60 +266,38 @@ let containerMix = function(obj, allowed = []) {
 		//if .allowed is [], then it can accept anything
 		//if .allowed is populated, then only those objects can be contained.
 		//1 if successful, 0 if not allowed, -1 if object already exists in contains
-		if (obj.contains.includes(newObj)) {
-			return -1;
-		}
-
-		if (obj.allowed.length == 0) {
+		if (newObj.belongsTo == obj) {return -1;} //already exists
+		if (obj.allowed.length == 0 || obj.allowed.includes(newObj.name[0])) {
 			//if .allowed is empty
 			if (newObj.belongsTo) {newObj.belongsTo.removeObject(newObj.name[0]);}; //remove object from previous container's .contains
 			newObj.belongsTo = this; //sets mew container
 			obj.contains.push(newObj);
 			return 1;
 		}
-		else {
-			//check if in .allowed
-			if (obj.allowed.includes(newObj.name[0])) {
-				if (newObj.belongsTo) {newObj.belongsTo.removeObject(newObj.name[0]);}; //remove object from previous container's .contains
-				newObj.belongsTo = this; //sets mew container
-				obj.contains.push(newObj);
-				return 1;
-			}
-			else {
-				return 0;
-			};
-		};
+		else {return 0;};
 	}
 	obj.removeObject = function(objString) {
 		//searches self.contains for object with objString in its .names
 		//if found, removes and returns the object
 		//returns 0 if not found, -1 if duplicate
 		var searchObj = findObjInArrayByName(objString, obj.contains);
-		if (searchObj[0] == 0) {
-			return 0;
-		}
-		else if (searchObj[0] == -1) {
-			return -1;
-		}
-		else {
-			return obj.contains.splice(searchObj[0][1],1);
-		};
+		if (searchObj[0] == 0) {return 0;}
+		else if (searchObj[0] == -1) {return -1;}
+		else {return obj.contains.splice(searchObj[1],1);};
 	}
 };
 
 //objects
 function basicObject(objInfo) {
 	//Makes a basic option with {name, description, important, takeable}
-	//properties: name, important, takeable
+	//properties: name, important, takeable, belongsTo
 	//methods: describeVerbose
 	this.name = objInfo.name || "noname";
 	this.important = objInfo.important || false;
 	this.takeable = objInfo.takeable || false;
 	this.description = objInfo.description || "It's either undescribeable or I forgot to describe this object sorry";
-	this.belongsTo || null;
-	this.describeVerbose = function() {
-		return this.description;
-	};
+	this.belongsTo = null;
+	this.describeVerbose = function() {return this.description;};
 };
 
 function roomObj() {
@@ -346,8 +320,7 @@ function roomObj() {
         //TODO: filter out only important things. Make a temporary array of all important things and then constructing the string.
         var importantObjects = [];
         for (var i=0; i < this.inanimates.length; i++) {
-        	if (this.inanimates[i].important) {
-        		importantObjects.push(this.inanimates[i].name[0]);
+        	if (this.inanimates[i].important) {importantObjects.push(this.inanimates[i].name[0]);
         	};
         };
 		var outString = "";
@@ -392,12 +365,8 @@ function personObj(name, userId) {
 	this.sayStuff = function() {console.log("stuff");}
     this.describeVerbose = function() {
         var outString = "";
-        if (this.id == userId) {
-            outString = "Hey look it's you.";
-        }
-        else {
-            outString = "It's " + this.name;
-        };
+        if (this.id == userId) {outString = "Hey look it's you.";}
+        else {outString = "It's " + this.name;};
         return outString;      
     };      
 };
@@ -408,152 +377,5 @@ function makeUserId() {
 }
 
 var sessionId = makeUserId();
-console.log(sessionId);
 var _user = new personObj('You', sessionId);
 containerMix(_user);
-
-
-
-
-
-
-
-
-
-//room population below
-var _room = new roomObj();
-var _lamp = new basicObject({name: ['lamp', 'light', 'bulb'], description:'foo', important: true});
-containerMix(_lamp, ['magenta filter', 'cyan filter','yellow filter']);
-_lamp.colorName = "white";
-_lamp.color = 9111111
-_lamp.describeVerbose = function() {
-	if (this.color != 9000000) {var outString = 'A small desk lamp.  Its ' + this.colorName + ' light illuminates the room.'}
-	else {var outString = "Only a little light is coming from it, just enough to barely light the room."}
-	var temp = objLister(this.contains);
-	if (temp[1] != '') {
-		outString += ' ' + capitalizeFirstLetter(temp[0] + ' ' + temp[1] + ' attached to the light bulb.'); 
-	};
-	return outString;
-};
-_lamp.addObject = function(newObj) {
-	//adds object to self.contains
-	//if .allowed is [], then it can accept anything
-	//if .allowed is populated, then only those objects can be contained.
-	//1 if successful, 0 if not allowed, -1 if object already exists in contains
-	if (this.contains.includes(newObj)) {
-		return -1;
-	}
-
-	if (this.allowed.includes(newObj.name[0])) {
-		if (newObj.belongsTo) {newObj.belongsTo.removeObject(newObj.name[0]);}; //remove object from previous container's .contains
-		newObj.belongsTo = this; //sets mew container
-		this.contains.push(newObj);
-			this.changeBackgroundColor(newObj.color);
-		return 1;
-	}
-	else {
-		return 0;
-	};
-}
-_lamp.removeObject = function(objString) {
-	//searches self.contains for object with objString in its .names
-	//if found, removes and returns the object
-	//returns 0 if not found, -1 if duplicate
-	var searchObj = findObjInArrayByName(objString, this.contains);
-	if (searchObj[0] == 0) {
-		return 0;
-	}
-	else if (searchObj[0] == -1) {
-		return -1;
-	}
-	else {
-		this.changeBackgroundColor(-1*searchObj[0][0].color);
-		return this.contains.splice(searchObj[0][1],1);
-
-	};
-}
-_lamp.changeBackgroundColor = function(color) {
-	this.color -= color
-	switch(this.color - 9000000) {
-		case 111111:
-			this.colorName = "white";
-			break;
-		case 111100:
-			this.colorName = "yellow";
-			break;
-		case 110011:
-			this.colorName = "magenta";
-			break;
-		case 1111:
-			this.colorName = "cyan";
-			break;
-		case 11:
-			this.colorName = "blue";
-			break;
-		case 1100:
-			this.colorName = "green";
-			break;
-		case 110000:
-			this.colorName = "red";
-			break;
-		case 0:
-			this.colorName = "black";
-			break;
-	};
-	var colorString = (this.color).toString();
-	colorString = colorString.replace(/1/g,"F");
-	colorString = colorString.replace(/9/,"#");
-	console.log(colorString);
-	document.body.style.backgroundColor = colorString;
-	var inputForm = document.getElementsByClassName('inputForm');
-	if (this.color - 9000000 == 0) {
-		document.body.style.color = "white";
-		inputForm[0].style.color = "white";
-	}
-	else {
-		document.body.style.color = "black";
-		inputForm[0].style.color = "black";
-	};
-};
-
-var _magentaFilter = new basicObject({name: ['magenta filter','filter','magenta'], description: "A transparent magenta filter.", takeable: true});
-_magentaFilter.color = 1100;
-var _cyanFilter = new basicObject({name: ['cyan filter','filter','cyan'], description: "A transparent cyan filter.", takeable: true});
-_cyanFilter.color = 110000;
-var _yellowFilter = new basicObject({name: ['yellow filter','filter','yellow'], description: "A transparent yellow filter.", takeable: true});
-_yellowFilter.color = 11;
-var _testStuff = new basicObject({name: ['test', 'test stuff', 'stuff'], description: 'Some stuff for testing.', takeable: true});
-var _aha = new basicObject({name: ['A-ha CD', 'me on', 'on me', 'aha','a-ha', 'cd'], description: 'A hit single by the 80s synthpop band A-ha.', takeable: true});
-
-var _table = new basicObject({name:['table','desk'], description:"A sturdy wooden table.", important: true});
-containerMix(_table);
-_table.addObject(_magentaFilter);
-_table.addObject(_cyanFilter);
-_table.addObject(_yellowFilter);
-_table.addObject(_lamp);
-_table.describeVerbose = function() {
-	var outString = "A sturdy wooden table.";
-	var temp = objLister(this.contains);
-	if (temp[1] != '') {
-		outString += ' ' + capitalizeFirstLetter(temp[0] + ' ' + temp[1] + ' on the table.' ); 
-	};
-	return outString;
-};
-
-var _wall = new basicObject({name: ['wall','walls'], description:'Large white plaster walls.'});
-var _ceiling = new basicObject({name: ['ceiling'], description: "A bland tiled ceiling. Someone should have put more effort into decorating this place!"});
-var _floor = new basicObject({name: ['floor','ground'], description: "Speckled linoleum."});
-
-_room.addObject(_lamp);
-_room.addObject(_table);
-_room.addObject(_wall);
-_room.addObject(_ceiling);
-_room.addObject(_floor);
-_room.addObject(_magentaFilter);
-_room.addObject(_cyanFilter);
-_room.addObject(_yellowFilter);
-_room.addObject(_testStuff);
-_room.addObject(_aha);
-_room.addPerson(_user);
-
-newMessage(_room.describe());
---
+++ scripts/populate.js
@@ -0,0 +1,139 @@
+//room population below
+var _room = new roomObj();
+var _lamp = new basicObject({name: ['lamp', 'light', 'bulb'], description:'foo', important: true});
+containerMix(_lamp, ['magenta filter', 'cyan filter','yellow filter']);
+_lamp.colorName = "white";
+_lamp.color = 9111111
+_lamp.describeVerbose = function() {
+	if (this.color != 9000000) {var outString = 'A small desk lamp.  Its ' + this.colorName + ' light illuminates the room.'}
+	else {var outString = "Only a little light is coming from it, just enough to barely light the room."}
+	var temp = objLister(this.contains);
+	if (temp[1] != '') {
+		outString += ' ' + capitalizeFirstLetter(temp[0] + ' ' + temp[1] + ' attached to the light bulb.'); 
+	};
+	return outString;
+};
+_lamp.addObject = function(newObj) {
+	//adds object to self.contains
+	//if .allowed is [], then it can accept anything
+	//if .allowed is populated, then only those objects can be contained.
+	//1 if successful, 0 if not allowed, -1 if object already exists in contains
+	if (this.contains.includes(newObj)) {
+		return -1;
+	}
+
+	if (this.allowed.includes(newObj.name[0])) {
+		if (newObj.belongsTo) {newObj.belongsTo.removeObject(newObj.name[0]);}; //remove object from previous container's .contains
+		newObj.belongsTo = this; //sets mew container
+		this.contains.push(newObj);
+			this.changeBackgroundColor(newObj.color);
+		return 1;
+	}
+	else {
+		return 0;
+	};
+}
+_lamp.removeObject = function(objString) {
+	//searches self.contains for object with objString in its .names
+	//if found, removes and returns the object
+	//returns 0 if not found, -1 if duplicate
+	var searchObj = findObjInArrayByName(objString, this.contains);
+	if (searchObj[0] == 0) {
+		return 0;
+	}
+	else if (searchObj[0] == -1) {
+		return -1;
+	}
+	else {
+		this.changeBackgroundColor(-1*searchObj[0].color);
+		return this.contains.splice(searchObj[1],1);
+
+	};
+}
+_lamp.changeBackgroundColor = function(color) {
+	this.color -= color
+	switch(this.color - 9000000) {
+		case 111111:
+			this.colorName = "white";
+			break;
+		case 111100:
+			this.colorName = "yellow";
+			break;
+		case 110011:
+			this.colorName = "magenta";
+			break;
+		case 1111:
+			this.colorName = "cyan";
+			break;
+		case 11:
+			this.colorName = "blue";
+			break;
+		case 1100:
+			this.colorName = "green";
+			break;
+		case 110000:
+			this.colorName = "red";
+			break;
+		case 0:
+			this.colorName = "black";
+			break;
+	};
+	var colorString = (this.color).toString();
+	colorString = colorString.replace(/1/g,"F");
+	colorString = colorString.replace(/9/,"#");
+	document.body.style.backgroundColor = colorString;
+	if (this.color - 9000000 == 0) {
+		document.body.style.color = "white";
+		document.getElementsByClassName('inputForm')[0].style.color = "white";
+	}
+	else {
+		document.body.style.color = "black";
+		document.getElementsByClassName('inputForm')[0].style.color = "black";
+	};
+};
+
+var _magentaFilter = new basicObject({name: ['magenta filter','filter','magenta'], description: "A transparent magenta filter.", takeable: true});
+_magentaFilter.color = 1100;
+var _cyanFilter = new basicObject({name: ['cyan filter','filter','cyan'], description: "A transparent cyan filter.", takeable: true});
+_cyanFilter.color = 110000;
+var _yellowFilter = new basicObject({name: ['yellow filter','filter','yellow'], description: "A transparent yellow filter.", takeable: true});
+_yellowFilter.color = 11;
+
+var _testStuff = new basicObject({name: ['test', 'test stuff', 'stuff'], description: 'Some stuff for testing.', takeable: true});
+var _aha = new basicObject({name: ['A-ha CD', 'me on', 'on me', 'aha','a-ha', 'cd'], description: 'A hit single by the 80s synthpop band A-ha.', takeable: true});
+
+var _table = new basicObject({name:['table','desk'], description:"A sturdy wooden table.", important: true});
+containerMix(_table);
+_table.addObject(_magentaFilter);
+_table.addObject(_cyanFilter);
+_table.addObject(_yellowFilter);
+_table.addObject(_lamp);
+_table.describeVerbose = function() {
+	var outString = "A sturdy wooden table.";
+	var temp = objLister(this.contains);
+	if (temp[1] != '') {
+		outString += ' ' + capitalizeFirstLetter(temp[0] + ' ' + temp[1] + ' on the table.' ); 
+	};
+	return outString;
+};
+
+var _wall = new basicObject({name: ['wall','walls'], description:'Large white plaster walls.'});
+var _ceiling = new basicObject({name: ['ceiling'], description: "A bland tiled ceiling. Someone should have put more effort into decorating this place!"});
+var _floor = new basicObject({name: ['floor','ground'], description: "Speckled linoleum."});
+var _door = new basicObject({name: ['door'], description: "There appears to be no door in this room. That might be a little concerning."});
+
+
+_room.addObject(_lamp);
+_room.addObject(_table);
+_room.addObject(_wall);
+_room.addObject(_ceiling);
+_room.addObject(_floor);
+_room.addObject(_door);
+_room.addObject(_magentaFilter);
+_room.addObject(_cyanFilter);
+_room.addObject(_yellowFilter);
+_room.addObject(_testStuff);
+_room.addObject(_aha);
+_room.addPerson(_user);
+
+newMessage(_room.describe());
